f9f7c214b81220a76b0bf4a4fabc738dd16c8aca
EthanFifle <ethan.fifle@gmail.com>
2023-02-03 19:10:33 -0500

    "V3 commit"

--- Visualizer.js
+++ Visualizer.js
@@ -1,24 +1,34 @@
 
 const questions = [
-    {
+    {//1
         question: "What word best describes the tone of the voice?", //Material for face
-        options: ["soft", "bright", "sad", "mellow","angry", "rough", "uptight", "smooth", "flat", "sharp"]
+        options: ["soft", "bright", "sad", "mellow","angry", "rough", "uptight", "smooth", "flat", "sharp"],
+        inputType: "radio"
     },
-    {
+    {//2
+        question: "When this voice appears, what are your feelings towards it? You can choose up to two feelings", //Colour
+        options: ["detached", "dependant", "tranquil", "disgust", "authoritative", "curious", "calm", "angry", "optimistic", "joy", "trusting", "loyal"],
+        inputType: "checkbox"
+    },
+    {//3
         question: "How seriously do you take what the voice is telling you? Is it very important or does it have very little meaning?",//Warm or Cool
-        options: ["very intrusive", "neutral", "not involved, very hands off"]
+        options: ["very intrusive", "neutral", "not involved, very hands off"],
+        inputType: "radio"
     },
-    {
+    {//4
         question: "How strict is the voice? 1 is least strict, 7 is extremely strict.", //Shape of head
-        options: ["one", "two", "three", "four","five", "six", "seven"]
+        options: ["one", "two", "three", "four","five", "six", "seven"],
+        inputType: "radio"
     },
-    {
+    {//5
         question: "What word best describes the mood that your intrusive inner voice is in?", //Eyes
-        options: ["happy ", "sad", "neutral", "angry","scared", "anxious", "manipulative", "jealous"]
+        options: ["happy ", "sad", "neutral", "angry","scared", "anxious", "manipulative", "jealous"],
+        inputType: "radio"
     },
-    {
+    {//6
         question: "How loud is the voice? 1 is quietest, 7 is loudest.",
-        options: ["one", "two", "three", "four","five", "six", "seven"]
+        options: ["one", "two", "three", "four","five", "six", "seven"],
+        inputType: "radio"
     }
 ];
 
@@ -35,8 +45,12 @@ function displayQuestion() {
     questionContainer.innerHTML = questions[currentQuestion].question;
     optionsContainer.innerHTML = questions[currentQuestion].options
         .map(
-            (option, index) =>
-                `<label id="label-${index}" class="option-label"><input type="radio" name="answer" value="${option}">${option}</label><br>`
+            (option, index) => {
+                let inputType = questions[currentQuestion].inputType === "checkbox" ? "checkbox" : "radio";
+                return `<label id="label-${index}" class="option-label">
+                        <input type="${inputType}" name="answer" value="${option}">${option}
+                        </label><br>`
+            }
         )
         .join("");
 
@@ -51,13 +65,34 @@ function displayQuestion() {
     });
 
     optionsContainer.onclick = function(event) {
-        if (event.target.type !== "radio") {
-            return;
-        }
         const selectedOption = event.target.value;
-        userAnswers[currentQuestion] = selectedOption;
-        nextQuestion();
-        displayThis(currentQuestion, questions[currentQuestion].options.indexOf(selectedOption));
+
+        if (event.target.type === "radio") {
+            userAnswers[currentQuestion] = selectedOption;
+            nextQuestion();
+            displayThis(currentQuestion, questions[currentQuestion].options.indexOf(selectedOption));
+        }else if (event.target.type === "checkbox") {
+            const selectedCheckboxes = optionsContainer.querySelectorAll("input[type=checkbox]:checked");
+            let subArray = [];
+
+            if (selectedCheckboxes.length <= 2) {
+                nextQuestion();
+
+                if (selectedCheckboxes.length === 1) {
+                    userAnswers[currentQuestion] = selectedOption;
+                    displayThis(currentQuestion, questions[currentQuestion].options.indexOf(selectedOption));
+                } else {
+                    selectedCheckboxes.forEach(checkbox => {
+                        subArray.push(checkbox.value);
+                    });
+                    userAnswers[currentQuestion] = subArray;
+                }
+
+
+            } else {
+                event.target.checked = false;
+            }
+        }
 
     };
 
@@ -89,7 +124,7 @@ function nextQuestion() {
 
 function displayThis(question, answer){
 
-    if(question === 0) {
+    if (question === 0) {
         switch (answer) {
             case 0:
                 images.push({src: "Layer 1 - Bagrounds/soft.png" });
@@ -123,7 +158,47 @@ function displayThis(question, answer){
                 break;
             default:
         }
-    }else if(question === 1){
+    } else if (question === 1){
+        switch(answer){
+            case 0:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_detached.png" });
+                break;
+            case 1:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_dependant.png" });
+                break;
+            case 2:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_tranquil.png" });
+                break;
+            case 3:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_disgust.png" });
+                break;
+            case 4:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_authoritative.png" });
+                break;
+            case 5:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_curious.png" });
+                break;
+            case 6:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_calm.png" });
+                break;
+            case 7:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_angry.png" });
+                break;
+            case 8:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_optimistic.png" });
+                break;
+            case 9:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_joy.png" });
+                break;
+            case 10:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_trusting.png" });
+                break;
+            case 11:
+                images.push({src: "Layer 2 - Colours/Single colours/Colours_loyal.png" });
+                break;
+            default:
+        }
+    } else if (question === 2){
         switch(answer){
             case 0:
                 images.push({src: "Layer 3 - Glows/Glows_warm.png" });
@@ -136,7 +211,7 @@ function displayThis(question, answer){
                 break;
             default:
         }
-    }else if(question === 2){
+    } else if (question === 3){
         switch(answer){
             case 0:
                 images.push({src: "Layer 4 - Face Shapes/one.png" });
@@ -161,7 +236,7 @@ function displayThis(question, answer){
                 break;
             default:
         }
-    }else if(question === 3){
+    } else if (question === 4){
         switch(answer){
             case 0:
                 images.push({src: "Layer 5 - Eyes/happy.png" });
@@ -189,7 +264,7 @@ function displayThis(question, answer){
                 break;
             default:
         }
-    }else{
+    } else {
         switch(answer){
             case 0:
                 images.push({src: "Layer 6 - Mouths/one.png" });
