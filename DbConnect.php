fd793f5e79e86d5634d48af2e75368374aa8480e
EthanFifle <ethan.fifle@gmail.com>
2023-03-17 12:17:07 -0400

    "V6 commit"

--- DbConnect.php
+++ DbConnect.php
@@ -20,19 +20,19 @@ $data = json_decode(file_get_contents("php://input"));
 
 // Insert data into database
 $sql = "INSERT INTO Image_Builder (Q1_Index, Q1_Ans,
-                             Q2a_Index, Q2a_Ans,
-                             Q2b_Index, Q2b_Ans,
-                             Q3_Index, Q3_Ans,
-                             Q4_Index, Q4_Ans,
-                             Q5_Index, Q5_Ans,
-                             Q6_Index, Q6_Ans,) 
-            VALUES ('$data->Index1', '$data->Ans1', 
-                    '$data->Index2a', '$data->Ans2a',
-                    '$data->Index2b', '$data->Ans2b',
-                    '$data->Index3', '$data->Ans3',
-                    '$data->Index4', '$data->Ans4',
-                    '$data->Index5', '$data->Ans5',
-                    '$data->Index6', '$data->Ans6',)";
+                                   Q2a_Index, Q2a_Ans,
+                                   Q2b_Index, Q2b_Ans,
+                                   Q3_Index, Q3_Ans,
+                                   Q4_Index, Q4_Ans,
+                                   Q5_Index, Q5_Ans,
+                                   Q6_Index, Q6_Ans) 
+            VALUES ('".$data->Index1."', '".$data->Ans1."', 
+                    '".$data->Index2a."', '".$data->Ans2a."',
+                    '".$data->Index2b."', '".$data->Ans2b."',
+                    '".$data->Index3."', '".$data->Ans3."',
+                    '".$data->Index4."', '".$data->Ans4."',
+                    '".$data->Index5."', '".$data->Ans5."',
+                    '".$data->Index6."', '".$data->Ans6."')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
